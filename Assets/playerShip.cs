b5f8cc1a31ba32fe2fc30f32f0dfab1e24dda7fe
colorzzr <colorzzr@gmail.com>
2019-03-24 23:24:15 -0400

    無限大な夢のあとの 何もない世の中じゃ

---
+++ Assets/playerShip.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class playerShip : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hz = CrossPlatformInputManager.GetAxis("Horizontal");
+        print(hz);
+    }
+}
